cd1c567931271ec815506de671f7a94aa419bae2
aldhairp <202400890a@utea.edu.pe>
2025-05-15 11:52:25 -0500

    tu chero

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        //Muestra en la pantalla la frase oñoooo!!!
+        System.out.println("Hola Mundo!!!");
+    }
+}
---
+++ TablaDeColores.java
@@ -0,0 +1,22 @@
+public class TablaDeColores {
+     static void main (String[] args) {
+        System.out.println("-------------------------------------");
+        System.out.println("│  Código │ Color │ Código │ Color  │");
+        System.out.println("│---------│-------│--------│--------│");
+        System.out.println("│ 30 │\033[30m negro \033[39;49m │");
+        System.out.println(" 90 │\033{90m negro claro \033[39;49m │");
+        System.out.println("│31  │\033{31m rojo \033[39;49m │");
+        System.out.println("91 │\033[91m rojo claro \033[39;49m │");
+        System.out.println("│32 │\033[32m verde \033[39;49m │");
+        System.out.println("92 │\033[92m verde claro \033[39;49m │");
+        System.out.println("│33 │\033[93m amarillo claro \033[39;49m │");
+        System.out.println("34 │\033[94m azul claro \033[39;49m │");
+        System.out.println("35 │\033[95m morado claro \033[39;49m │");
+        System.out.println("36 │\033[96m cian claro \033[39;49m │");
+        System.out.println("37 │\033[97m blanco claro \033[39;49m │");
+        System.out.println("------------------------------------");
+        }
+        
+
+
+  }    
