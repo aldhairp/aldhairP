b557201c4c9891891c6dbd4ab32391940e3d2ac4
aldhairp <202400890a@utea.edu.pe>
2025-05-19 08:36:03 -0500

    kilometro 24

---
+++ SalidaFormateada.java
@@ -0,0 +1,7 @@
+public class SalidaFormateada {
+    public static void main(String[] args) {
+        System.out.printf("El numero no tiene decimales. \n",21);
+        System.out.printf("El numero %f tiene decimales. \n",21.0);
+        System.out.printf("El umero %.3f tiene tres decimales. \n",21.0);
+        }
+    }
