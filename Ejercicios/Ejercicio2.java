ef123afb8efa6fd2b2957ee7c3fe047dbe53cf91
aldhairp <202400890a@utea.edu.pe>
2025-05-19 09:43:05 -0500

    sirenita

---
+++ Ejercicios/Ejercicio2.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Dirección\tNumero");
+        System.out.println("--------\t-------");
+        System.out.println("av.machupichu \t98215154");
+    }
+    
+}
---
+++ Ejercicios/Ejercicio3.java
@@ -0,0 +1,21 @@
+package Ejercicios;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("------------------------");
+        System.out.println("│  Ingles │ Castellano │");
+        System.out.println("│---------│------------");
+        System.out.println("   House     \tCasa    ");
+        System.out.println("   Dog     \tPerro    ");
+        System.out.println("   Book     \tLibro    ");
+        System.out.println("   Friend     \tAmigo    ");
+        System.out.println("   Happy     \tFeliz    ");
+        System.out.println("   Tree     \tArból    ");
+        System.out.println("   School     \tEscuela    ");
+        System.out.println("   Car     \tAuto    ");
+        System.out.println("   Water     \tAgua    ");
+        System.out.println("   Chair     \tSilla    ");
+
+    }
+    
+}
